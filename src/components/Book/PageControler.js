2942d6903fcebcdd90f4e4f297ce40b0784504da
Tagir <tagirman998@gmail.com>
2021-02-23 11:48:57 +0400

    fixed some styles and refactor

--- src/components/Book/PageControler.js
+++ src/components/Book/PageControler.js
@@ -11,21 +11,21 @@ const PageController = observer(() => {
 
   return (
     <div className={styles.controllerContainer}>
-
-      <div
-        className={cn(bookStore.isFullScreen && styles.hidingControlFooter, styles.controlFooter)}> {/*todo rename styles*/}
-        <div className={styles.footerControl}>
+      <div className={cn(bookStore.isFullScreen && styles.hidingControlFooter, styles.controlFooter)}>
+        <div className={styles.pageControlContainer}>
           <Button onClick={bookStore.currentBook.prevPage} className={styles.pageControlButton} type={"text"}
                   size={"large"}><LeftOutlined/></Button>
           <Button onClick={bookStore.currentBook.nextPage} className={styles.pageControlButton} type={"text"}
                   size={"large"}><RightOutlined/></Button>
         </div>
       </div>
-      <Button className={styles.exitFullScreenBtn} onClick={() =>
-        document.exitFullscreen()
-      }>
-        <FullscreenExitOutlined/>
-      </Button>
+      <div className={styles.exitFullScreenContainer}>
+        <Button className={styles.exitFullScreenBtn} onClick={() =>
+          document.exitFullscreen()
+        }>
+          <FullscreenExitOutlined/>
+        </Button>
+      </div>
     </div>
   )
 
